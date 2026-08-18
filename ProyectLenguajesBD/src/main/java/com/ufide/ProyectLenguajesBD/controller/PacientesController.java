214bdc136f7f88ce2ed050c97ba6b9fc71287aab
dono-byte <donovanbonillacruz@gmail.com>
2026-08-18 15:42:59 -0600

    Feat: error de avance compañeros acotado, era que no encontraba los css porque estaban dentro de templates

--- ProyectLenguajesBD/src/main/java/com/ufide/ProyectLenguajesBD/controller/PacientesController.java
+++ ProyectLenguajesBD/src/main/java/com/ufide/ProyectLenguajesBD/controller/PacientesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.ufide.ProyectLenguajesBD.service.PacienteService;
@@ -38,13 +39,13 @@ public class PacientesController {
     }
 
     @GetMapping("/pacientes/editar/{id}")
-    public String editarPaciente(@RequestParam Integer id, Model model) {
+    public String editarPaciente(@PathVariable Integer id, Model model) {
         model.addAttribute("paciente", pacienteService.obtenerPorId(id).orElse(null));
         return "pacientes";
     }
 
     @GetMapping("/pacientes/eliminar/{id}")
-    public String eliminarPaciente(@RequestParam Integer id) {
+    public String eliminarPaciente(@PathVariable Integer id) {
         pacienteService.eliminar(id);
         return "redirect:/pacientes";
     }
